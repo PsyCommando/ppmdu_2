3efd7c63cc0c38b1e9b2e79b42e073ea296a53ce
PsyCommando <PsyCommando@users.noreply.github.com>
2023-01-29 19:56:12 -0500

    Update midi_fmtrule.hpp

--- ppmdu_2/include/ext_fmts/midi_fmtrule.hpp
+++ ppmdu_2/include/ext_fmts/midi_fmtrule.hpp
@@ -3,6 +3,12 @@
 /*
 Including this instanciate a format ID for the MIDI file format in the file format analyser.
 */
+#include <string>
+
+namespace audio
+{
+    extern const std::string MIDI_FILE_EXT;
+};
 
 #ifdef USE_PPMDU_CONTENT_TYPE_ANALYSER
     #include <types/content_type_analyser.hpp>
